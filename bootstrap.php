4a404cbf9173dc8e625a18ebc4158d017f731b3e
Philipp Scheit <p.scheit@ps-webforge.com>
2013-11-07 11:38:46 +0100

    refactor bootstrap and add virtual file stream

--- bootstrap.php
+++ bootstrap.php
@@ -10,7 +10,7 @@ use Psc\Boot\BootLoader;
 $ds = DIRECTORY_SEPARATOR;
 
 require_once __DIR__.$ds.'lib'.$ds.'package.boot.php';
-$bootLoader = new BootLoader(__DIR__);
+$bootLoader = new BootLoader(__DIR__, 'Webforge\Setup\BootContainer');
 $bootLoader->loadComposer();
-$bootLoader->registerContainer();
+return $bootLoader->registerContainer();
 //$bootLoader->registerPackageRoot();
