dd7b7bf7fc8823a978d4560a06c2e42e56f2ffee
Philipp Scheit <p.scheit@ps-webforge.com>
2013-11-13 11:50:56 +0100

    create the schema after every model test is complete

--- model-tests/ModelDCValidationTest.php
+++ model-tests/ModelDCValidationTest.php
@@ -3,8 +3,9 @@
 namespace Webforge\Doctrine\Compiler;
 
 use Doctrine\ORM\Tools\SchemaValidator;
+use Webforge\Doctrine\Util;
 
-class ModelDCValidationTest extends \Webforge\Doctrine\Compiler\Test\Base {
+class ModelDCValidationTest extends \Webforge\Doctrine\Compiler\Test\ModelBase {
 
   public function setUp() {
     $this->chainClass = __NAMESPACE__ . '\\Compiler';
@@ -13,12 +14,28 @@ class ModelDCValidationTest extends \Webforge\Doctrine\Compiler\Test\Base {
     $this->validator = new SchemaValidator($this->em);
   }
 
-  protected function setUpPackage() {
-    $this->blogPackage = self::$package;
-  }
-
   public function testSchemaIsValid() {
     $errors = $this->validator->validateMapping();
     $this->assertCount(0, $errors, 'There were errors with the generated schema: '.print_r($errors, true));
+  }
+
+  public function testCreatingTheSchema() {
+    // this will work on travis out of the box but locally we have to delete all tables
+    $connection = $this->em->getConnection();
+    $sm = $connection->getSchemaManager();
+
+    $connection->query('set foreign_key_checks=0');
+    foreach ($sm->listTableNames() as $tableName) {
+      $sm->dropTable($tableName);
+    }
+    $connection->query('set foreign_key_checks=1');
+
+    $sql = $this->dcc->getUtil()->updateSchema('tests', Util::FORCE, $eol = "\n");
+
+    foreach (TestReflection::tableNames() as $params) {
+      list($table) = $params;
+
+      $this->assertContains('CREATE TABLE '.$table, $sql);
+    }
   }
 }
